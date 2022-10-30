b8abc1cd15f200aab0982b176ea1a5d255d4ce34
Benjamín <benjaalejandro206@gmail.com>
2022-10-30 09:38:18 -0300

    build(plugin): added maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("com.github.johnrengelman.shadow") version("7.1.2")
 	`java-library`
+	`maven-publish`
 }
 
 val directory = property("group") as String
@@ -17,6 +18,18 @@ dependencies {
 	
 	implementation("org.jetbrains:annotations:23.0.0")
 	implementation("commons-lang:commons-lang:2.6")
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			groupId = "net.xconfig"
+			artifactId = "XConfig"
+			version = release
+			
+			from(components["java"])
+		}
+	}
 }
 
 tasks {
