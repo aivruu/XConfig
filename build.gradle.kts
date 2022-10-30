a5f0aff3b6eaa254677b5f05b07db43ab06903c4
InitSync <benjaalejandro206@gmail.com>
2022-10-30 12:23:35 -0300

    build: added bungeecord-api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,13 @@ val libs = property("libs") as String
 
 repositories {
 	maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+	maven("https://oss.sonatype.org/content/groups/public/")
 	mavenCentral()
 }
 
 dependencies {
 	compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+	compileOnly("net.md-5:bungeecord-api:1.19-R0.1-SNAPSHOT")
 	
 	implementation("org.jetbrains:annotations:23.0.0")
 	implementation("commons-lang:commons-lang:2.6")
