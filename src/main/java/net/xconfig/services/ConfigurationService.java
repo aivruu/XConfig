2a5c961438a5f2115ae05f561827bca0cfa4e0d5
InitSync <benjaalejandro206@gmail.com>
2022-10-30 17:53:28 -0300

    feat(configuration-service): added methods for manager and handler for BungeeCord platforms

--- src/main/java/net/xconfig/services/ConfigurationService.java
+++ src/main/java/net/xconfig/services/ConfigurationService.java
@@ -1,10 +1,12 @@
 package net.xconfig.services;
 
+import net.md_5.bungee.api.plugin.Plugin;
 import net.xconfig.bukkit.config.BukkitConfigurationModel;
 import net.xconfig.bukkit.impls.BukkitConfigurationHandlerImpl;
 import net.xconfig.bukkit.impls.BukkitConfigurationModelImpl;
 import net.xconfig.bungee.config.BungeeConfigurationModel;
 import net.xconfig.bungee.impls.BungeeConfigurationHandlerImpl;
+import net.xconfig.bungee.impls.BungeeConfigurationModelImpl;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -18,7 +20,7 @@ import org.jetbrains.annotations.NotNull;
  */
 public interface ConfigurationService {
 	/**
-	 * Returns a new instance of ManagerModel.
+	 * Returns a new instance of BukkitConfigurationModelImpl.
 	 *
 	 * @param plugin A JavaPlugin instance required for the files creation.
 	 * @return A ManagerModel instance.
@@ -29,7 +31,7 @@ public interface ConfigurationService {
 	}
 	
 	/**
-	 * Returns a new instance of ManagerModel.
+	 * Returns a new instance of BukkitConfigurationHandlerImpl.
 	 *
 	 * @param model A ConfigurationModel instance.
 	 * @return A ManagerModel instance.
@@ -39,7 +41,25 @@ public interface ConfigurationService {
 		return new BukkitConfigurationHandlerImpl(model);
 	}
 	
-	static BungeeConfigurationHandlerImpl bungeeHandler(@NotNull BungeeConfigurationModel model) {
+	/**
+	 * Returns a new BungeeConfigurationModelImpl object.
+	 *
+	 * @param plugin A Plugin instance required for the files creation.
+	 * @return A BukkitConfigurationModelImpl object.
+	 */
+	@Contract ("_ -> new")
+	static @NotNull BungeeConfigurationModelImpl bungeeManager(@NotNull Plugin plugin) {
+		return new BungeeConfigurationModelImpl(plugin);
+	}
+	
+	/**
+	 * Returns a new instance of BungeeConfigurationHandlerImpl.
+	 *
+	 * @param model A BungeeConfigurationModel object.
+	 * @return A BungeeConfigurationHandlerImpl object.
+	 */
+	@Contract (value = "_ -> new", pure = true)
+	static @NotNull BungeeConfigurationHandlerImpl bungeeHandler(@NotNull BungeeConfigurationModel model) {
 		return new BungeeConfigurationHandlerImpl(model);
 	}
 }
