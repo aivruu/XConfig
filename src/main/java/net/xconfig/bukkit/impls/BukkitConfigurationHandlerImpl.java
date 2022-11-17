26516999325a8b6a9efa70d6a998e25bf2a6dacc
InitSync <benjaalejandro206@gmail.com>
2022-11-17 13:12:03 -0300

    feat(BukkitConfigurationHandlerImpl): bump up version to 1.0.5, add checks to some methods for verify if their return values has null

--- src/main/java/net/xconfig/bukkit/impls/BukkitConfigurationHandlerImpl.java
+++ src/main/java/net/xconfig/bukkit/impls/BukkitConfigurationHandlerImpl.java
@@ -2,28 +2,34 @@ package net.xconfig.bukkit.impls;
 
 import net.xconfig.bukkit.config.BukkitConfigurationHandler;
 import net.xconfig.bukkit.config.BukkitConfigurationModel;
+import net.xconfig.bukkit.utils.TextUtils;
 import net.xconfig.enums.Action;
 import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 /**
  * Class to handle the configuration files and get values from that files.
  *
  * @author InitSync
- * @version 1.0.4
+ * @version 1.0.5
  * @since 1.0.0
  * @see net.xconfig.bukkit.config.BukkitConfigurationHandler
  */
 public final class BukkitConfigurationHandlerImpl implements BukkitConfigurationHandler {
 	private final BukkitConfigurationModel configuration;
+	private final Logger logger;
 	
 	public BukkitConfigurationHandlerImpl(@NotNull BukkitConfigurationModel configuration) {
 		this.configuration = Objects.requireNonNull(configuration, "The ConfigurationModel object is null.");
+		this.logger = Bukkit.getLogger();
 	}
 	
 	/**
@@ -52,8 +58,9 @@ public final class BukkitConfigurationHandlerImpl implements BukkitConfiguration
 				this.configuration.save(fileName);
 				break;
 			case WRITE:
-				assert toPath != null && !toPath.isEmpty();
-				assert object != null;
+				if (toPath == null) throw new NullPointerException("The path requested is null.");
+				if (object == null) throw new NullPointerException("The object to set is null.");
+				
 				this.configuration
 					.file(fileName)
 					.set(toPath, object);
@@ -72,9 +79,15 @@ public final class BukkitConfigurationHandlerImpl implements BukkitConfiguration
 	public @NotNull String text(@NotNull String fileName, @NotNull String path) {
 		Validate.notEmpty(fileName, "The file name is empty.");
 		
-		return this.configuration
+		String text = this.configuration
 			.file(fileName)
 			.getString(path);
+		if (text == null) {
+			this.logger.severe("Cannot get the string from the path '" + path + "'. Please check the configuration file.");
+			return "";
+		}
+		
+		return TextUtils.colorize(text);
 	}
 	
 	/**
@@ -104,9 +117,15 @@ public final class BukkitConfigurationHandlerImpl implements BukkitConfiguration
 	public @Nullable Object any(@NotNull String fileName, @NotNull String path) {
 		Validate.notEmpty(fileName, "The file name is empty.");
 		
-		return this.configuration
+		Object object = this.configuration
 			.file(fileName)
 			.get(path);
+		if (object == null) {
+			this.logger.severe("Cannot get the object from '" + path + "'. Please check the path.");
+			return null;
+		}
+		
+		return object;
 	}
 	
 	/**
@@ -120,9 +139,15 @@ public final class BukkitConfigurationHandlerImpl implements BukkitConfiguration
 	public @NotNull List<?> list(@NotNull String fileName, @NotNull String path) {
 		Validate.notEmpty(fileName, "The file name is empty.");
 		
-		return this.configuration
+		List<?> list = this.configuration
 			.file(fileName)
 			.getList(path);
+		if (list == null) {
+			this.logger.severe("Cannot found the list requested from '" + path + "'. Check the configuration file.");
+			return Collections.emptyList();
+		}
+		
+		return list;
 	}
 	
 	/**
@@ -136,9 +161,9 @@ public final class BukkitConfigurationHandlerImpl implements BukkitConfiguration
 	public @NotNull List<String> textList(@NotNull String fileName, @NotNull String path) {
 		Validate.notEmpty(fileName, "The file name is empty.");
 		
-		return this.configuration
+		return TextUtils.colorize(this.configuration
 			.file(fileName)
-			.getStringList(path);
+			.getStringList(path));
 	}
 	
 	/**
@@ -168,8 +193,14 @@ public final class BukkitConfigurationHandlerImpl implements BukkitConfiguration
 	public @Nullable ConfigurationSection configSection(@NotNull String fileName, @NotNull String path) {
 		Validate.notEmpty(fileName, "The file name is empty.");
 		
-		return this.configuration
+		ConfigurationSection section = this.configuration
 			.file(fileName)
 			.getConfigurationSection(path);
+		if (section == null) {
+			this.logger.severe("The ConfigurationSection object requested in '" + path + "' is null, please check the configuration file.");
+			return null;
+		}
+		
+		return section;
 	}
 }
