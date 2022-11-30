38b5b273b39a378b9513d144116c891ca035314d
InitSync <benjaalejandro206@gmail.com>
2022-11-29 21:21:14 -0300

    refactor(BungeeConfigurationModel): make as default to the methods build() and delete()

--- src/main/java/net/xconfig/bungee/config/BungeeConfigurationModel.java
+++ src/main/java/net/xconfig/bungee/config/BungeeConfigurationModel.java
@@ -6,10 +6,29 @@ import net.md_5.bungee.config.Configuration;
  * Configuration Interface for the Bungee implementations.
  *
  * @author InitSync
- * @version 1.1.0
+ * @version 1.1.1
  * @since 1.0.1
  */
 public interface BungeeConfigurationModel {
+	/**
+	 * Creates and loads multiple files.
+	 *
+	 * @param folderName Name of the folder.
+	 * @param files Names of the files.
+	 */
+	default void build(String folderName, String... files) {
+		for (String file : files) this.build(folderName, file);
+	}
+	
+	/**
+	 * Delete one or more files.
+	 *
+	 * @param files Names of files to delete.
+	 */
+	default void delete(String... files) {
+		for (String file : files) this.delete(file);
+	}
+	
 	/**
 	 * Returns a Configuration object using the file name, if the file doesn't exist, will be
 	 * return null.
@@ -28,26 +47,11 @@ public interface BungeeConfigurationModel {
 	void build(String folderName, String fileName);
 	
 	/**
-	 * Creates and loads multiple files.
-	 *
-	 * @param folderName Name of the folder.
-	 * @param files Names of the files.
-	 */
-	void build(String folderName, String... files);
-	
-	/**
 	 * Delete a file.
 	 *
 	 * @param fileName Name of file.
 	 */
 	void delete(String fileName);
-	
-	/**
-	 * Delete one or more files.
-	 *
-	 * @param files Names of files to delete.
-	 */
-	void delete(String... files);
 	
 	/**
 	 * Reloads a file.
