8d9de3c7d11a2a44124d04697b1ed457be998b48
InitSync <benjaalejandro206@gmail.com>
2022-12-10 12:45:34 -0300

    chore(TextUtils): bump up version value to 1.1.23

--- src/main/java/net/xconfig/bukkit/utils/TextUtils.java
+++ src/main/java/net/xconfig/bukkit/utils/TextUtils.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
  * Utility class to colorize strings at Bukkit (Spigot/Paper) platforms.
  *
  * @author InitSync
- * @version 1.1.2
+ * @version 1.1.23
  * @since 1.0.5
  */
 public final class TextUtils {
--- src/main/java/net/xconfig/bungee/utils/TextUtils.java
+++ src/main/java/net/xconfig/bungee/utils/TextUtils.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
  * Utility class to colorize strings at BungeeCord platforms.
  *
  * @author InitSync
- * @version 1.1.2
+ * @version 1.1.23
  * @since 1.0.5
  */
 public final class TextUtils {
