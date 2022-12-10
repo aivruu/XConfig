06bb63f46f661ea16c06dc4ed859fad429932c6a
InitSync <benjaalejandro206@gmail.com>
2022-12-10 12:44:42 -0300

    feat(BungeeConfigurationModelImpl): add way for get files using their folder name

--- src/main/java/net/xconfig/bungee/impls/BungeeConfigurationModelImpl.java
+++ src/main/java/net/xconfig/bungee/impls/BungeeConfigurationModelImpl.java
@@ -1,6 +1,9 @@
 package net.xconfig.bungee.impls;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
@@ -11,8 +14,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
@@ -20,40 +21,42 @@ import java.util.logging.Logger;
  * Implementation that manages the creation of the files.
  *
  * @author InitSync
- * @version 1.1.2
+ * @version 1.1.23
  * @since 1.0.1
  * @see net.xconfig.bungee.config.BungeeConfigurationModel
  */
-public final class BungeeConfigurationModelImpl implements BungeeConfigurationModel {
+public final class BungeeConfigurationModelImpl
+implements BungeeConfigurationModel {
 	private final Plugin plugin;
 	private final Logger logger;
-	private final Map<String, File> files;
-	private final Map<String, Configuration> configurations;
+	private final Table<String, String, File> files;
+	private final Table<String, String, Configuration> configurations;
 	
-	public BungeeConfigurationModelImpl(Plugin plugin, Logger logger) {
+	public BungeeConfigurationModelImpl(Plugin plugin) {
 		this.plugin = Objects.requireNonNull(plugin, "The Plugin instance is null.");
-		this.logger = Objects.requireNonNull(logger, "The Logger object is null.");
-		this.files = new HashMap<>();
-		this.configurations = new HashMap<>();
+		this.logger = ProxyServer.getInstance().getLogger();
+		this.files = HashBasedTable.create();
+		this.configurations = HashBasedTable.create();
 	}
 	
 	/**
-	 * Returns a Configuration object using the file name, if the file doesn't exist, will be
+	 * Returns a Configuration object using the file name and their name (if there are a folder), if the file doesn't exist, will be
 	 * return null.
 	 *
+	 * @param folderName Name of the folder.
 	 * @param fileName Name of file.
 	 * @return A Configuration object.
 	 */
 	@Override
-	public Configuration file(String fileName) {
+	public Configuration file(String folderName, String fileName) {
 		Preconditions.checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!files.containsKey(fileName) && !configurations.containsKey(fileName)) {
+		if (!files.contains(folderName, fileName) && !configurations.contains(folderName, fileName)) {
 			logger.severe("Cannot get the file " + fileName + " because doesn't exist.");
 			return null;
 		}
 		
-		return configurations.get(fileName);
+		return configurations.get(folderName, fileName);
 	}
 	
 	/**
@@ -90,8 +93,8 @@ public final class BungeeConfigurationModelImpl implements BungeeConfigurationMo
 			}
 		}
 		
-		files.put(fileName, file);
-		try { configurations.put(fileName, ConfigurationProvider.getProvider(YamlConfiguration.class).load(file)); }
+		files.put(folderName, fileName, file);
+		try { configurations.put(fileName, folderName, ConfigurationProvider.getProvider(YamlConfiguration.class).load(file)); }
 		catch (IOException exception) {
 			logger.severe("Cannot load the file '" + fileName + "'.");
 			exception.printStackTrace();
@@ -101,36 +104,41 @@ public final class BungeeConfigurationModelImpl implements BungeeConfigurationMo
 	/**
 	 * Delete a file.
 	 *
+	 * @param folderName Name of the folder.
 	 * @param fileName Name of file.
 	 */
 	@Override
-	public void delete(String fileName) {
+	public void delete(String folderName, String fileName) {
 		Preconditions.checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		File file = files.get(fileName);
-		if (file == null) {
+		if (!files.contains(folderName, fileName) && !configurations.contains(folderName, fileName)) {
 			logger.severe("Cannot delete the file '" + fileName + "' because doesn't exist.");
 			return;
 		}
 		
-		if (!file.delete()) logger.severe("Cannot delete the file '" + fileName + "'.");
+		try { files.get(folderName, fileName).delete(); }
+		catch (SecurityException exception) {
+			logger.severe("Cannot delete the file '" + fileName + "'.");
+			exception.printStackTrace();
+		}
 	}
 	
 	/**
 	 * Reloads a file.
 	 *
+	 * @param folderName Name of the folder.
 	 * @param fileName Name of file.
 	 */
 	@Override
-	public void reload(String fileName) {
+	public void reload(String folderName, String fileName) {
 		Preconditions.checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!files.containsKey(fileName) && !configurations.containsKey(fileName)) {
-			logger.severe("Cannot reload the file " + fileName + " because doesn't exist.");
+		if (!files.contains(folderName, fileName) && !configurations.contains(folderName, fileName)) {
+			logger.severe("Cannot reload the file '" + fileName + "' because doesn't exist.");
 			return;
 		}
 		
-		try { ConfigurationProvider.getProvider(YamlConfiguration.class).load(files.get(fileName)); }
+		try { ConfigurationProvider.getProvider(YamlConfiguration.class).load(files.get(folderName, fileName)); }
 		catch (IOException exception) {
 			logger.severe("Cannot reload the file '" + fileName + "'.");
 			exception.printStackTrace();
@@ -140,19 +148,24 @@ public final class BungeeConfigurationModelImpl implements BungeeConfigurationMo
 	/**
 	 * Saves an existing file.
 	 *
+	 * @param folderName Name of the folder.
 	 * @param fileName Name of file.
 	 */
 	@Override
-	public void save(String fileName) {
+	public void save(String folderName, String fileName) {
 		Preconditions.checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!files.containsKey(fileName) && !configurations.containsKey(fileName)) {
+		if (!files.contains(folderName, fileName) && !configurations.contains(folderName, fileName)) {
 			logger.severe("Cannot save the file " + fileName + " because doesn't exist.");
 			return;
 		}
 		
-		try { ConfigurationProvider.getProvider(YamlConfiguration.class).save(configurations.get(fileName), files.get(fileName)); }
-		catch (IOException exception) {
+		try {
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(
+				 configurations.get(folderName, fileName),
+				 files.get(folderName, fileName)
+			);
+		} catch (IOException exception) {
 			logger.severe("Failed to save the file" + fileName + ".");
 			exception.printStackTrace();
 		}
