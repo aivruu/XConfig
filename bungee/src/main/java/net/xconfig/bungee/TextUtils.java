563bdc05fdaac316d37543b6180636c7320f8788
InitSync <benjaalejandro206@gmail.com>
2023-01-16 22:55:54 -0300

    chore(TextUtils): move from package, add new `containsIgnoreCase()` method and optimize `colorize()` method and improve her structure

---
+++ bungee/src/main/java/net/xconfig/bungee/TextUtils.java
@@ -0,0 +1,103 @@
+package net.xconfig.bungee;
+
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Utility class to colorize strings at BungeeCord platforms.
+ *
+ * @author InitSync
+ * @version 1.1.4
+ * @since 1.0.5
+ */
+public final class TextUtils {
+	private static final Pattern HEX_PATTERN = Pattern.compile("#[a-fA-F0-9]{6}}");
+	private static final StringBuilder BUILDER = new StringBuilder();
+	
+	private TextUtils() {}
+	
+	/**
+	 * Checks if the sequence given contains the next sequence specified without import if are Case Sensitive.
+	 *
+	 * @param target The string to check.
+	 * @param search The string to search into the string target specified.
+	 * @return True if the sequence was found, else return false.
+	 */
+	public static boolean containsIgnoreCase(String target, String search) {
+		if (target == null || search == null) return false;
+		
+		int length = search.length();
+		int max = target.length() - length;
+		
+		for (int i = 0 ; i < max ; i++) {
+			if (target.regionMatches(true, i, search, 0, length)) return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Colorize the list content.
+	 *
+	 * @param textList A List of text.
+	 * @return The list colorized.
+	 */
+	public static List<String> colorize(List<String> textList) {
+		return textList.stream()
+			.map(TextUtils::colorize)
+			.collect(Collectors.toList());
+	}
+	
+	/**
+	 * Colorize the string passed as parameter, if the server version protocol is lower than 16 (-1.16),
+	 * will colorize with the normal colors, overwise, will apply the HEX colors.
+	 * <p>
+	 * [!] Original code taken out from SternalBoard repository:
+	 * https://github.com/ShieldCommunity/SternalBoard/blob/main/plugin/src/main/java/com/xism4/
+	 * sternalboard/utils/PlaceholderUtils.java
+	 *
+	 * @param text Text to colorize.
+	 * @return Text colorized.
+	 */
+	public static String colorize(String text) {
+		String[] parts = text.split(String.format("((?<=%1$s)|(?=%1$s))", "&"));
+		Matcher matcher = HEX_PATTERN.matcher(text);
+		
+		if (!text.contains("&#")) {
+			while (matcher.find()) {
+				text = text.replace(
+					 text.substring(matcher.start(), matcher.end()),
+					 String.valueOf(ChatColor.of(text.substring(matcher.start(), matcher.end())))
+				);
+				matcher = HEX_PATTERN.matcher(text);
+			}
+			
+			BUILDER.append(text);
+			return BUILDER.toString();
+		}
+		
+		for (int i = 0 ; i < parts.length ; i++) {
+			if (parts[i].equals("&")) {
+				i++;
+				if (parts[i].charAt(0) == '#') BUILDER.append(ChatColor.of(parts[i].substring(0, 7)));
+				else BUILDER.append(ChatColor.translateAlternateColorCodes('&', "&" + parts[i]));
+			} else {
+				while (matcher.find()) {
+					parts[i] = parts[i].replace(
+						 parts[i].substring(matcher.start(), matcher.end()),
+						 String.valueOf(ChatColor.of(parts[i].substring(matcher.start(), matcher.end())))
+					);
+					matcher = HEX_PATTERN.matcher(text);
+				}
+				
+				BUILDER.append(parts[i]);
+			}
+		}
+		
+		return BUILDER.toString();
+	}
+}
