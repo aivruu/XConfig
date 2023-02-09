20f6940d3ff72dd6f0fde8e794aae5411dd3949c
InitSync <benjaalejandro206@gmail.com>
2023-02-09 17:44:54 -0300

    Improve and simplify `SimpleConfigurationManager` code structure using `YamlFile` cached objects for create, load/reload and save the files. Remove folder name parameter at `ConfigurationManager#get` method and remove `ConfigurationManager#buildCustom` method, using for it the parameter `custom` on the original method

--- bukkit/src/main/java/net/xconfig/bukkit/model/SimpleConfigurationManager.java
+++ bukkit/src/main/java/net/xconfig/bukkit/model/SimpleConfigurationManager.java
@@ -1,157 +1,117 @@
 package net.xconfig.bukkit.model;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import net.xconfig.bukkit.model.config.ConfigurationManager;
+import net.xconfig.bukkit.model.objects.YamlFile;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static org.bukkit.Bukkit.getLogger;
 
 /**
- * Class that handles internally the files creation.
+ * Class that handles internally the file creation.
  *
  * @author InitSync
- * @version 1.1.4
+ * @version 1.1.5
  * @since 1.0.0
  * @see ConfigurationManager
  */
 public class SimpleConfigurationManager implements ConfigurationManager {
 	private final JavaPlugin plugin;
-	private final Table<String, String, File> files;
-	private final Table<String, String, FileConfiguration> configurations;
+	private final Map<String, YamlFile> cachedFiles;
 	
 	public SimpleConfigurationManager(JavaPlugin plugin) {
 		this.plugin = Objects.requireNonNull(plugin, "The plugin instance is null.");
-		this.files = HashBasedTable.create();
-		this.configurations = HashBasedTable.create();
+		cachedFiles = new HashMap<>();
 	}
 	
 	@Override
-	public FileConfiguration file(String folderName, String fileName) {
+	public FileConfiguration get(String fileName) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!exists(folderName, fileName)) {
+		final YamlFile file = cachedFiles.get(fileName);
+		if (file == null || !file.file().exists()) {
 			getLogger().severe("Cannot get the file " + fileName + " because doesn't exist.");
 			return null;
 		}
 		
-		return configurations.get(folderName, fileName);
+		return file.get();
 	}
 	
 	@Override
-	public void build(String folderName, String fileName) {
+	public void build(String folderName, String fileName, boolean custom) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		File file;
-		
-		if (folderName.isEmpty()) file = new File(plugin.getDataFolder(), fileName);
-		else file = new File(plugin.getDataFolder() + File.separator + folderName + File.separator, fileName);
-		
-		if (!file.exists()) {
-			if (folderName.isEmpty()) plugin.saveResource(fileName, false);
-			else plugin.saveResource(folderName + File.separator + fileName, false);
-		}
-		
-		files.put(folderName, fileName, file);
-		configurations.put(folderName, fileName, YamlConfiguration.loadConfiguration(file));
-	}
-	
-	@Override
-	public void buildCustom(String folderName, String fileName) {
-		checkArgument(!fileName.isEmpty(), "The file name is empty.");
-		
-		if (!plugin.getDataFolder().exists()) plugin.getDataFolder().mkdir();
-		
-		File file;
-		
-		if (folderName.isEmpty()) {
-			file = new File(plugin.getDataFolder(), fileName);
-			if (!file.exists()) {
-				try { file.createNewFile(); }
-				catch (IOException exception) {
-					getLogger().severe("Cannot create the custom file '" + fileName + "'.");
-					exception.printStackTrace();
-					return;
-				}
-			}
-		} else {
-			new File(plugin.getDataFolder(), folderName).mkdir();
-			file = new File(plugin.getDataFolder() + File.separator + folderName + File.separator, fileName);
-			if (!file.exists()) {
-				try { file.createNewFile(); }
-				catch (IOException exception) {
-					getLogger().severe("Cannot create the custom file '" + fileName + "'.");
-					exception.printStackTrace();
-					return;
-				}
-			}
-		}
-		
-		files.put(folderName, fileName, file);
-		configurations.put(folderName, fileName, YamlConfiguration.loadConfiguration(file));
-	}
-	
-	@Override
-	public void delete(String folderName, String fileName) {
-		checkArgument(!fileName.isEmpty(), "The file name is empty.");
-		
-		if (!exists(folderName, fileName)) {
-			getLogger().severe("Cannot delete the file '" + fileName + "' because doesn't exist.");
+		final YamlFile file = cachedFiles.put(fileName, new YamlFile(plugin, folderName, fileName));
+		if (file == null) {
+			getLogger().severe("Cannot create the file " + fileName + "!");
 			return;
 		}
 		
-		try { files.get(folderName, fileName).delete(); }
+		if (custom) {
+			file.createAsCustom();
+			return;
+		}
+		
+		file.create();
+	}
+	
+	@Override
+	public void delete(String fileName) {
+		checkArgument(!fileName.isEmpty(), "The file name is empty.");
+		
+		YamlFile file = cachedFiles.remove(fileName);
+		if (file == null || !file.file().exists()) {
+			getLogger().severe("Cannot delete the file " + fileName + " because already doesn't exist.");
+			return;
+		}
+		
+		try { file.file().delete(); }
 		catch (SecurityException exception) {
 			getLogger().severe("Cannot delete the file '" + fileName + "'.");
 			exception.printStackTrace();
 		}
 		
-		files.remove(folderName, fileName);
-		configurations.remove(folderName, fileName);
+		file = null;
 	}
 	
 	@Override
-	public void reload(String folderName, String fileName) {
+	public void reload(String fileName) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!exists(folderName, fileName)) {
-			getLogger().severe("Cannot reload the file '" + fileName + "' because doesn't exist.");
+		final YamlFile file = cachedFiles.get(fileName);
+		if (file == null || !file.file().exists()) {
+			getLogger().severe("Cannot reload the file " + fileName + " because doesn't exist.");
 			return;
 		}
 		
-		try { configurations.get(folderName, fileName).load(files.get(folderName, fileName)); }
+		try { file.get().load(file.file()); }
 		catch (InvalidConfigurationException | IOException exception) {
-			getLogger().severe("Failed to reload the file" + fileName + ".");
+			getLogger().severe("Failed to load the file" + fileName + ".");
 			exception.printStackTrace();
 		}
 	}
 	
 	@Override
-	public void save(String folderName, String fileName) {
+	public void save(String fileName) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!exists(folderName, fileName)) {
+		final YamlFile file = cachedFiles.get(fileName);
+		if (file == null || !file.file().exists()) {
 			getLogger().severe("Cannot save the file " + fileName + " because doesn't exist.");
 			return;
 		}
 		
-		try { configurations.get(folderName, fileName).save(files.get(folderName, fileName)); }
+		try { file.get().save(file.file()); }
 		catch (IOException exception) {
 			getLogger().severe("Failed to save the file" + fileName + ".");
 			exception.printStackTrace();
 		}
-	}
-	
-	@Override
-	public boolean exists(String folderName, String fileName) {
-		return files.contains(folderName, fileName) && configurations.contains(folderName, fileName);
 	}
 }
--- bungee/src/main/java/net/xconfig/bungee/model/SimpleConfigurationManager.java
+++ bungee/src/main/java/net/xconfig/bungee/model/SimpleConfigurationManager.java
@@ -1,17 +1,15 @@
 package net.xconfig.bungee.model;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 import net.xconfig.bungee.model.config.ConfigurationManager;
+import net.xconfig.bungee.model.objects.YamlFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -21,163 +19,97 @@ import static net.md_5.bungee.api.ProxyServer.getInstance;
  * Implementation that manages the creation of the files.
  *
  * @author InitSync
- * @version 1.1.4
+ * @version 1.1.5
  * @since 1.0.1
  * @see ConfigurationManager
  */
 public final class SimpleConfigurationManager implements ConfigurationManager {
 	private final Plugin plugin;
-	private final Table<String, String, File> files;
-	private final Table<String, String, Configuration> configurations;
+	private final Map<String, YamlFile> cachedFiles;
 	
 	public SimpleConfigurationManager(Plugin plugin) {
 		this.plugin = Objects.requireNonNull(plugin, "The Plugin instance is null.");
-		this.files = HashBasedTable.create();
-		this.configurations = HashBasedTable.create();
+		this.cachedFiles = new HashMap<>();
 	}
 	
 	@Override
-	public Configuration file(String folderName, String fileName) {
+	public Configuration get(String fileName) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!exists(folderName, fileName)) {
+		final YamlFile file = cachedFiles.get(fileName);
+		if (file == null || !file.file().exists()) {
 			getInstance().getLogger().severe("Cannot get the file " + fileName + " because doesn't exist.");
 			return null;
 		}
 		
-		return configurations.get(folderName, fileName);
+		return file.get();
 	}
 	
 	@Override
-	public void build(String folderName, String fileName) {
+	public void build(String folderName, String fileName, boolean custom) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		File file;
-		
-		if (folderName.isEmpty()) file = new File(plugin.getDataFolder(), fileName);
-		else file = new File(plugin.getDataFolder() + File.separator + folderName + File.separator, fileName);
-		
-		if (!file.exists()) {
-			InputStream inputFile;
-			if (folderName.isEmpty()) inputFile = plugin.getResourceAsStream(fileName);
-			else inputFile = plugin.getResourceAsStream(folderName + File.separator + file);
-			
-			if (inputFile == null) {
-				getInstance().getLogger().severe("The resource '" + fileName + "' isn't inside of plugin jar file!");
-				return;
-			}
-			
-			try { Files.copy(inputFile, file.toPath()); }
-			catch (IOException exception) {
-				getInstance().getLogger().severe("Cannot create the file '" + fileName + "'.");
-				exception.printStackTrace();
-			}
-		}
-		
-		files.put(folderName, fileName, file);
-		try { configurations.put(fileName, folderName, ConfigurationProvider.getProvider(YamlConfiguration.class).load(file)); }
-		catch (IOException exception) {
-			getInstance().getLogger().severe("Cannot load the file '" + fileName + "'.");
-			exception.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void buildCustom(String folderName, String fileName) {
-		checkArgument(!fileName.isEmpty(), "The file name is empty.");
-		
-		if (!plugin.getDataFolder().exists()) plugin.getDataFolder().mkdir();
-		
-		File file;
-		
-		if (folderName.isEmpty()) {
-			file = new File(plugin.getDataFolder(), fileName);
-			if (!file.exists()) {
-				try { file.createNewFile(); }
-				catch (IOException exception) {
-					getInstance().getLogger().severe("Cannot create the custom file '" + fileName + "'.");
-					exception.printStackTrace();
-					return;
-				}
-			}
-		} else {
-			new File(plugin.getDataFolder(), folderName).mkdir();
-			file = new File(plugin.getDataFolder() + File.separator + folderName + File.separator, fileName);
-			if (!file.exists()) {
-				try { file.createNewFile(); }
-				catch (IOException exception) {
-					getInstance().getLogger().severe("Cannot create the custom file '" + fileName + "'.");
-					exception.printStackTrace();
-					return;
-				}
-			}
-		}
-		
-		files.put(folderName, fileName, file);
-		try { configurations.put(fileName, folderName, ConfigurationProvider.getProvider(YamlConfiguration.class).load(file)); }
-		catch (IOException exception) {
-			getInstance().getLogger().severe("Cannot load the file '" + fileName + "'.");
-			exception.printStackTrace();
-		}
-	}
-	
-	@Override
-	public void delete(String folderName, String fileName) {
-		checkArgument(!fileName.isEmpty(), "The file name is empty.");
-		
-		if (!exists(folderName, fileName)) {
-			getInstance().getLogger().severe("Cannot delete the file '" + fileName + "' because doesn't exist.");
+		final YamlFile file = cachedFiles.put(fileName, new YamlFile(plugin, folderName, fileName));
+		if (file == null || !file.file().exists()) {
+			getInstance().getLogger().severe("Cannot create the file " + fileName + "!");
 			return;
 		}
 		
-		try { files.get(folderName, fileName).delete(); }
+		if (custom) {
+			file.createAsCustom();
+			return;
+		}
+		
+		file.create();
+	}
+	
+	@Override
+	public void delete(String fileName) {
+		checkArgument(!fileName.isEmpty(), "The file name is empty.");
+		
+		YamlFile file = cachedFiles.remove(fileName);
+		if (file == null || !file.file().exists()) {
+			getInstance().getLogger().severe("Cannot delete the file '" + fileName + "' because already doesn't exist.");
+			return;
+		}
+		
+		try { file.file().delete(); }
 		catch (SecurityException exception) {
 			getInstance().getLogger().severe("Cannot delete the file '" + fileName + "'.");
 			exception.printStackTrace();
 		}
 		
-		files.remove(folderName, fileName);
-		configurations.remove(folderName, fileName);
+		file = null;
 	}
 	
 	@Override
-	public void reload(String folderName, String fileName) {
+	public void reload(String fileName) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!exists(folderName, fileName)) {
+		final YamlFile file = cachedFiles.get(fileName);
+		if (file == null || !file.file().exists()) {
 			getInstance().getLogger().severe("Cannot reload the file '" + fileName + "' because doesn't exist.");
 			return;
 		}
 		
-		try { ConfigurationProvider.getProvider(YamlConfiguration.class).load(files.get(folderName, fileName)); }
-		catch (IOException exception) {
-			getInstance().getLogger().severe("Cannot reload the file '" + fileName + "'.");
-			exception.printStackTrace();
-		}
+		file.load();
 	}
 	
 	@Override
-	public void save(String folderName, String fileName) {
+	public void save(String fileName) {
 		checkArgument(!fileName.isEmpty(), "The file name is empty.");
 		
-		if (!exists(folderName, fileName)) {
+		final YamlFile file = cachedFiles.get(fileName);
+		if (file == null || !file.file().exists()) {
 			getInstance().getLogger().severe("Cannot save the file " + fileName + " because doesn't exist.");
 			return;
 		}
 		
 		try {
-			ConfigurationProvider.getProvider(YamlConfiguration.class).save(
-				 configurations.get(folderName, fileName),
-				 files.get(folderName, fileName)
-			);
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(file.get(), file.file());
 		} catch (IOException exception) {
 			getInstance().getLogger().severe("Failed to save the file" + fileName + ".");
 			exception.printStackTrace();
 		}
-	}
-	
-	@Override
-	public boolean exists(String folderName, String fileName) {
-		return files.contains(folderName, fileName) && configurations.contains(folderName, fileName);
 	}
 }
